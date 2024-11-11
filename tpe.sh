927fcb12646c613500ae3f35d235f6cc23f20756
cyelmini <cyelmini@itba.edu.ar>
2024-11-11 17:09:54 -0300

    agregue parametro api_key

--- tpe.sh
+++ tpe.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-# Verificar que se haya proporcionado el parámetro congress_number
-if [ -z "$1" ]; then
-  echo "Error: No congress_number provided."
-  echo "Usage: ./tpe.sh <congress_number>"
+# Verificamos que se pasen congress_number y api_key como parametros
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Error: Faltan parámetros."
+  echo "Usage: ./tpe.sh <congress_number> <api_key>"
   exit 1
 fi
 
-# Asignar el parámetro a una variable
+# Guardamos los valores en variables y vemos que el numero de congreso sea válido
 congress_number=$1
+api_key=$2
 
-# Verificar que el número esté dentro del rango permitido (1 a 118)
+# Verificamos que el número de congreso esté dentro del rango permitido (1 a 118)
 if ((congress_number < 1 || congress_number > 118)); then
   echo "Error: congress_number must be between 1 and 118."
   exit 1
 fi
 
-# Obtener congress_info.xml usando el parámetro congress_number
-curl -X GET "https://api.congress.gov/v3/congress/$congress_number?format=xml&api_key=$CONGRESS_API" \
+# Obtenemos congress_info.xml llamando al método GET
+curl -X GET "https://api.congress.gov/v3/congress/$congress_number?format=xml&api_key=$api_key" \
 -H "accept: application/xml" -o congress_info.xml
 
-# Obtener congress_members_info.xml usando el parámetro congress_number
-curl -X GET "https://api.congress.gov/v3/member/congress/$congress_number?format=xml&currentMember=false&limit=500&api_key=$CONGRESS_API" \
+# Obtenemos congress_members_info.xml llamando al método GET
+curl -X GET "https://api.congress.gov/v3/member/congress/$congress_number?format=xml&currentMember=false&limit=500&api_key=$api_key" \
 -H "accept: application/xml" -o congress_members_info.xml
 
 echo "Archivos congress_info.xml y congress_members_info.xml generados para el Congreso $congress_number"
